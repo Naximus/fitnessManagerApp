327a2e85204566d78365ddb5768581c41e0c1f13
Georgi Karchev <georgi.karchev@gmail.com>
2023-01-05 13:48:40 +0200

    Add scss to index.js & app.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
@@ -7,7 +7,8 @@ function App() {
     name: 'Стоян Димитров',
     phone: '0888',
     email: 'stoyan@abv.bg',
-    created: '2022.12.01'
+    created: '2022.12.01',
+    photo: null
   };
 
   return (
